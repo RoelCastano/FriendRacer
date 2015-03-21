83ce8c70172f9bedfc954870ac4c33ccfd4e1e7b
PatoBeltran <pbelsal@gmail.com>
2015-03-21 16:03:17 -0600

    Add first 4sq implementation

--- Podfile
+++ Podfile
@@ -4,10 +4,8 @@
 source 'https://github.com/CocoaPods/Specs.git'
 
 target 'Roady' do
-
+    pod "AMSlideMenu", "~> 1.5.4"
 end
 
 target 'RoadyTests' do
-
 end
-
--- Roady/ViewController.m
+++ Roady/ViewController.m
@@ -7,9 +7,14 @@
 //
 
 #import "ViewController.h"
+#import "RoadyCore.h"
+#import "BZFoursquare.h"
 
-@interface ViewController ()
-
+@interface ViewController () <BZFoursquareRequestDelegate>
+@property(nonatomic,strong) BZFoursquareRequest *request;
+@property(nonatomic,copy) NSDictionary *meta;
+@property(nonatomic,copy) NSArray *notifications;
+@property(nonatomic,copy) NSDictionary *response;
 @end
 
 @implementation ViewController
@@ -23,5 +28,56 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+#pragma mark -Foursquare search
+
+- (void)searchVenues {
+    [self prepareForRequest];
+    NSDictionary *parameters = @{@"ll": @"40.7,-74"};
+    self.request = [[RoadyCore sharedInstance].foursquare requestWithPath:@"venues/search" HTTPMethod:@"GET" parameters:parameters delegate:self];
+    [self.request start];
+//    [self updateView];
+    [UIApplication sharedApplication].networkActivityIndicatorVisible = YES;
+}
+
+- (void)cancelRequest {
+    if (_request) {
+        _request.delegate = nil;
+        [_request cancel];
+        self.request = nil;
+        [UIApplication sharedApplication].networkActivityIndicatorVisible = NO;
+    }
+}
+
+- (void)prepareForRequest {
+    [self cancelRequest];
+    self.meta = nil;
+    self.notifications = nil;
+    self.response = nil;
+}
+
+#pragma mark - BZFoursquareRequestDelegate
+
+- (void)requestDidFinishLoading:(BZFoursquareRequest *)request {
+    self.meta = request.meta;
+    self.notifications = request.notifications;
+    self.response = request.response;
+    self.request = nil;
+//    [self updateView];
+    [UIApplication sharedApplication].networkActivityIndicatorVisible = NO;
+}
+
+- (void)request:(BZFoursquareRequest *)request didFailWithError:(NSError *)error {
+    NSLog(@"%s: %@", __PRETTY_FUNCTION__, error);
+    UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:nil message:[error userInfo][@"errorDetail"] delegate:nil cancelButtonTitle:NSLocalizedString(@"OK", @"") otherButtonTitles:nil];
+    [alertView show];
+    self.meta = request.meta;
+    self.notifications = request.notifications;
+    self.response = request.response;
+    self.request = nil;
+//    [self updateView];
+    [UIApplication sharedApplication].networkActivityIndicatorVisible = NO;
+}
+
 
 @end
