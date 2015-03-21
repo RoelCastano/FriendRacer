e08246ca92fb4de9e8054594b827129e7d247cc5
PatoBeltran <pbelsal@gmail.com>
2015-03-21 14:34:27 -0600

    Initial Commit

---
+++ Roady/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Roady
+//
+//  Created by Patricio Beltrán on 3/21/15.
+//  Copyright (c) 2015 Da Team. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Roady/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Roady
+//
+//  Created by Patricio Beltrán on 3/21/15.
+//  Copyright (c) 2015 Da Team. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
